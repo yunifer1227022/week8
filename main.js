c74cbbd99d3336244a87697b29208852791a910c
klay <ads1029384756@gmail.com>
2023-08-30 12:51:53 +0800

    add swiper

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  enabled:false,
+  // Scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints:{
+    768:{
+      slidesPerView: 2,
+      enabled: false
+    },
+    992:{
+      slidesPerView: 3,
+      enabled: true
+    }
+  },
+  // 使 swiper 可以使用滑鼠滾輪捲動
+  mousewheel: true,
+});
+
+if(slidesPerView<3){
+  swiper.disable();
+}
